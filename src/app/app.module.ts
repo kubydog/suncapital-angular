3a994892cc09127d1cd666567cd35d80a86009f6
chenshuai.huang <chenshuai.huang@digihealthinnovations.com>
2019-02-01 15:17:48 +1100

    Management Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { DashboardComponent } from './management/dashboard/dashboard.component';
-import { NavigationComponent } from './management/navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './management/menu/menu.component';
 
 @NgModule({
   declarations: [
@@ -19,10 +16,7 @@ import { MenuComponent } from './management/menu/menu.component';
     HeaderComponent,
     LoginComponent,
     RegistrationComponent,
-    DashboardComponent,
-    NavigationComponent,
-    HomeComponent,
-    MenuComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
